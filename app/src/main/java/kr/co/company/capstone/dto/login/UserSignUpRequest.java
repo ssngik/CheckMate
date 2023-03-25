28aa97c7d71b6e6f55caac4eb7ea27d49917f8eb
김상익 <ksik518@naver.com>
2023-03-25 17:34:04 +0900

    API 명세에 따라 회원가입 필드 변경     userIdentifier -> identifier

--- app/src/main/java/kr/co/company/capstone/dto/login/UserSignUpRequest.java
+++ app/src/main/java/kr/co/company/capstone/dto/login/UserSignUpRequest.java
@@ -11,12 +11,12 @@ import lombok.NoArgsConstructor;
 // TODO: 2023/03/24 Builder 사용
 public class UserSignUpRequest implements Serializable{
 
-    private String userIdentifier;
+    private String identifier;
     private String username;
     private String emailAddress;
     private String nickname;
         public UserSignUpRequest(String userIdentifier, String username, String email, String nickname) {
-        this.userIdentifier = userIdentifier;
+        this.identifier = userIdentifier;
         this.username = username;
         this.emailAddress = email;
         this.nickname=nickname;
