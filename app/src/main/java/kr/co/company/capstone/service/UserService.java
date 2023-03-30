2bd77d6f1c6a12675c9497ab44d6541fcc2bcea3
김상익 <ksik518@naver.com>
2023-03-30 23:34:01 +0900

    로그인 통합에 따른 통합 로그인, 통합 회원가입 메소드 추가, 소셜 로그인 메소드 삭제 (kakao, naver, google)

--- app/src/main/java/kr/co/company/capstone/service/UserService.java
+++ app/src/main/java/kr/co/company/capstone/service/UserService.java
@@ -3,6 +3,8 @@ package kr.co.company.capstone.service;
 import kr.co.company.capstone.dto.UserNicknameUpdateRequest;
 import kr.co.company.capstone.dto.goal.GoalInfoListResponse;
 import kr.co.company.capstone.dto.goal.HistoryGoalInfoResponse;
+import kr.co.company.capstone.dto.login.LoginRequest;
+import kr.co.company.capstone.dto.login.LoginResponse;
 import kr.co.company.capstone.dto.login.UserSignUpRequest;
 import retrofit2.Call;
 import retrofit2.http.*;
@@ -11,14 +13,13 @@ public interface UserService {
     @GET("/users/exists")
     Call<Void> duplicatedNicknameCheck(@Query("nickname") String nickname);
 
-    @POST("/users/kakao") // LoginResponse
-    Call<Void> KakaoSignUp(@Body UserSignUpRequest request);
+    // 로그인
+    @POST("/users/login")
+    Call<LoginResponse> login(@Body LoginRequest loginRequest);
 
-//    @POST("/users/naver")
-//    Call<LoginResponse> SignUp(@Body UserSignUpRequest request);
-//
-//    @POST("/users/google")
-//    Call<LoginResponse> SignUp(@Body UserSignUpRequest request);
+    // 회원가입
+    @POST("/users")
+    Call<Void> signUp(@Body UserSignUpRequest request);
 
     @PATCH("/users/nickname")
     Call<Void> updateNickname(@Body UserNicknameUpdateRequest request);
