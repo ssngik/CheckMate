1729efeee7c88c6366f3cb217512a751ae26a184
김상익 <ksik518@naver.com>
2023-03-30 23:59:32 +0900

    Lombok의 @Builder 어노테이션을 사용하여 LoginRequest DTO를 리팩토링하고, @AllArgsConstructor와 @NoArgsConstructor 어노테이션을 삭제함

--- app/src/main/java/kr/co/company/capstone/dto/login/LoginRequest.java
+++ app/src/main/java/kr/co/company/capstone/dto/login/LoginRequest.java
@@ -3,11 +3,14 @@ package kr.co.company.capstone.dto.login;
 import lombok.*;
 
 @Getter
-@AllArgsConstructor //  모든 필드를 매개변수로 받는 생성자를 자동으로 생성
-@NoArgsConstructor // 매개변수가 없는 기본 생성자를 자동으로 생성
-
 public class LoginRequest {
-    private String providerId;
-    private String fcmToken;
+    private final String identifier;
+    private final String fcmToken;
+
+    @Builder
+    public LoginRequest(String identifier, String fcmToken){
+        this.identifier = identifier;
+        this.fcmToken = fcmToken;
+    }
 }
 
