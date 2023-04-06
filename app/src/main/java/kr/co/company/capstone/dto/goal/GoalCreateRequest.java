0843a405ea2eb30f77bada0ac8cf962f5ee46595
김상익 <ksik518@naver.com>
2023-04-06 17:30:08 +0900

    "GoalCreateRequest DTO : goalMethod 및 minimum 필드를 삭제하고, weekdays 필드를 checkDays 필드로 변경"

--- app/src/main/java/kr/co/company/capstone/dto/goal/GoalCreateRequest.java
+++ app/src/main/java/kr/co/company/capstone/dto/goal/GoalCreateRequest.java
@@ -3,15 +3,16 @@ package kr.co.company.capstone.dto.goal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+// 새 목표 생성
 @Data
 @AllArgsConstructor
+
 public class GoalCreateRequest {
     private String category;
     private String title;
-    private String goalMethod;
     private String startDate;
     private String endDate;
+    private String checkDays;
     private String appointmentTime;
-    private String weekDays;
-    private Integer minimumLike;
+
 }
