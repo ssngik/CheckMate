c3a660c60666ac66d8fc8217438352f723905a8b
김상익 <ksik518@naver.com>
2023-04-11 16:46:42 +0900

    [Refactor]     - 목표 생성 후 객체를 넘겨 받아 상세 정보 표시     - import 문 그룹화     - 즉시/확인 인증 없어짐에 따라 checkInstantOrConfirm 삭제     - API 변경에 따라 myLikeSet, myMethodSet 삭제     - 변수명 변경

--- app/src/main/java/kr/co/company/capstone/fragment/GoalCreateCompleteFragment.java
+++ app/src/main/java/kr/co/company/capstone/fragment/GoalCreateCompleteFragment.java
@@ -1,26 +1,35 @@
+// 안드로이드 OS 관련
 package kr.co.company.capstone.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+// 안드로이드 위젯 관련
 import android.widget.ImageView;
 import android.widget.TextView;
 
+// androidx 라이브러리 관련
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+// DTO 클래스 관련
+import kr.co.company.capstone.dto.goal.GoalCreateRequest;
+
+// R 클래스 관련
 import kr.co.company.capstone.R;
-import kr.co.company.capstone.dto.goal.GoalCreateResponse;
 
 public class GoalCreateCompleteFragment extends Fragment {
     String LOG_TAG = "Complete";
     private static final String ARG_PARAM1 = "goalId";
 
-    GoalCreateResponse goalCreateResponse;
+    GoalCreateRequest goalCreateRequest;
     ImageView myTitleImage;
+
+    Long goalId;
+
     public GoalCreateCompleteFragment() {
         // Required empty public constructor
     }
@@ -31,7 +40,8 @@ public class GoalCreateCompleteFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if(getArguments()!=null){
             Bundle bundle = getArguments();
-            goalCreateResponse = (GoalCreateResponse) bundle.getSerializable("goalCreateResponse");
+            goalId = bundle.getLong("goalId");
+            goalCreateRequest = (GoalCreateRequest) bundle.getSerializable("request");
         }
 }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -39,36 +49,32 @@ public class GoalCreateCompleteFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_complete, container, false);
 
 
-        TextView myTitleName = view.findViewById(R.id.my_title_name);
+        TextView title = view.findViewById(R.id.my_title_name);
         TextView startDateInfo = view.findViewById(R.id.start_date_info);
         TextView endDateInfo = view.findViewById(R.id.end_date_info);
-        TextView myDaySet = view.findViewById(R.id.my_day_set);
-        TextView myMethodSet = view.findViewById(R.id.my_method_set);
-        TextView myTimeSet = view.findViewById(R.id.my_time_set);
+        TextView checkDays = view.findViewById(R.id.my_day_set);
+        TextView appointmentTime = view.findViewById(R.id.my_time_set);
         TextView toMain = view.findViewById(R.id.to_main);
         TextView toInvite = view.findViewById(R.id.to_invite);
         myTitleImage = view.findViewById(R.id.my_title_image);
-        TextView myLikeSet = view.findViewById(R.id.my_like_set);
 
-        setTextWithGoalCreateResponse(myTitleName, startDateInfo, endDateInfo, myDaySet, myTimeSet, myLikeSet);
-        checkInstantOrConfirm(myMethodSet);
+        setTextWithGoalCreateRequest(title, startDateInfo, endDateInfo, checkDays, appointmentTime);
         clickInviteButton(toInvite);
         clickToMainButton(toMain);
 
         return view;
     }
 
-    private void setTextWithGoalCreateResponse(TextView myTitleName, TextView startDateInfo, TextView endDateInfo, TextView myDaySet, TextView myTimeSet, TextView myLikeSet) {
-        myTitleName.setText(goalCreateResponse.getTitle());
-        startDateInfo.setText(goalCreateResponse.getStartDate());
-        endDateInfo.setText(goalCreateResponse.getEndDate());
-        myDaySet.setText(goalCreateResponse.getWeekDays());
-        myLikeSet.setText(goalCreateResponse.getMinimumLike() != null ? goalCreateResponse.getMinimumLike().toString() : "");
-        if(goalCreateResponse.getAppointmentTime()!=null){
-            myTimeSet.setText(goalCreateResponse.getAppointmentTime());
-        }
+    // 목표 생성시 작성한 정보
+    private void setTextWithGoalCreateRequest(TextView title, TextView startDateInfo, TextView endDateInfo, TextView checkDays, TextView appointmentTime) {
+        title.setText(goalCreateRequest.getTitle());
+        startDateInfo.setText(goalCreateRequest.getStartDate());
+        endDateInfo.setText(goalCreateRequest.getEndDate());
+        checkDays.setText(goalCreateRequest.getCheckDays());
+        if(goalCreateRequest.getAppointmentTime()!=null) appointmentTime.setText(goalCreateRequest.getAppointmentTime());
     }
 
+    // Main 화면 이동
     private void clickToMainButton(TextView toMain) {
         toMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,22 +84,16 @@ public class GoalCreateCompleteFragment extends Fragment {
         });
     }
 
+    // 초대하기
     private void clickInviteButton(TextView toInvite) {
         toInvite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Bundle inviteId = new Bundle();
-                inviteId.putLong("goalId", goalCreateResponse.getId());
+                inviteId.putLong("goalId", goalId);
                 Navigation.findNavController(view).navigate(R.id.action_goalCreateCompleteActivity_to_fragmentInviteUser, inviteId);
             }
         });
     }
 
-    private void checkInstantOrConfirm(TextView myMethodSet) {
-        if(goalCreateResponse.getGoalMethod().equals("INSTANT")){
-            myMethodSet.setText("즉시 인증");
-        }else{
-            myMethodSet.setText("확인 후 인증");
-        }
-    }
 }
