465cde9ea923246adce223b1cf7506f5b8cae7a6
김상익 <ksik518@naver.com>
2023-04-16 17:14:43 +0900

    API 명세 변경에 따라 필드 변경     - goalMethod 삭제     - isChecked 삭제

--- app/src/main/java/kr/co/company/capstone/dto/goal/SimpleGoalInfoResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/goal/SimpleGoalInfoResponse.java
@@ -3,15 +3,10 @@ package kr.co.company.capstone.dto.goal;
 import lombok.Data;
 
 @Data
-public class SimpleGoalInfoResponse extends GoalInfo{
+public class SimpleGoalInfoResponse extends OngoingGoalInfo {
     private long id;
-    private String category;
     private String title;
-    private String goalMethod;
+    private String category;
     private String weekDays;
 
-    @Override
-    public boolean isChecked() {
-        return false;
-    }
 }
