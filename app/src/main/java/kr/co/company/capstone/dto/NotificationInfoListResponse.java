89927e222f2d66e9991153b7e6e52b1fb24538d7
김상익 <ksik518@naver.com>
2023-04-21 22:17:30 +0900

    API 명세 변경에 따른 코드 변경

--- app/src/main/java/kr/co/company/capstone/dto/NotificationInfoListResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/NotificationInfoListResponse.java
@@ -1,16 +1,10 @@
 package kr.co.company.capstone.dto;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
-public class NotificationInfoListResponse {
-    private List<NotificationInfoResponse> values;
-    private boolean hasNext;
+public class NotificationInfoListResponse<T> {
+    private List<T> notifications;
+    private boolean hasNext; // 조회할 수 있는 다음 페이지가 있는지 여부
 }
