e90d7f7b30790700a48c02c5d8d4568fd90ec2d4
김상익 <ksik518@naver.com>
2023-04-25 23:52:43 +0900

    팀원 초대에 대한 수락/거부 복구 - Response 파일 분기/ 파일명 변경   - TeamMateInviteResponse.java 삭제   - TeamMateInviteReplyResponse -> TeamMateAccpetInviteResponse,TeamMateRejectInviteResponse 로 분기

--- app/src/main/java/kr/co/company/capstone/dto/team_mate/TeamMateAcceptInviteResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/team_mate/TeamMateAcceptInviteResponse.java
@@ -2,8 +2,9 @@ package kr.co.company.capstone.dto.team_mate;
 
 import lombok.Data;
 
+// 팀원 초대에 대한 수락
 @Data
-public class TeamMateInviteReplyResponse {
+public class TeamMateAcceptInviteResponse {
     private Long goalId; // 목표 ID
     private Long mateId; // 팀원 ID
 }
---
+++ app/src/main/java/kr/co/company/capstone/dto/team_mate/TeamMateRejectInviteResponse.java
@@ -0,0 +1,8 @@
+package kr.co.company.capstone.dto.team_mate;
+
+import lombok.Data;
+
+@Data
+public class TeamMateRejectInviteResponse {
+    private Long notificationId; // 알림 ID
+}
