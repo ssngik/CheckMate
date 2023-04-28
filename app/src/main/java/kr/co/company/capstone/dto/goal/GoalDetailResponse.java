244c590df6ff91a279be8ce5e2bb42cd8a58dda3
김상익 <ksik518@naver.com>
2023-04-28 22:11:22 +0900

    API 명세에 따라 DTO 수정   - goalStatus -> status   - goalMethod 삭제   - goalSchedule, mateSchedule, progress 추가

--- app/src/main/java/kr/co/company/capstone/dto/goal/GoalDetailResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/goal/GoalDetailResponse.java
@@ -8,17 +8,18 @@ import lombok.Data;
 
 @Data
 public class GoalDetailResponse<TeamMatesResponse> implements Serializable {
-    private long goalId;
-    private List<TeamMatesResponse> mates;
-    private String category;
-    private String title;
-    private String goalMethod;
+    private long goalId; // 목표 ID
+    private String category; // 카테고리
+    private String title; // 목표 이름
     private String startDate;
     private String endDate;
     private String weekDays;
     private String appointmentTime;
-    private String goalStatus;
-    private boolean inviteable;
-    private Integer minimumLike;
-    private Uploadable uploadable;
+    private String status;
+    private List<TeamMatesResponse> mates; // 목표에 속한 팀원들
+    private boolean inviteable; // 초대할 수 있는 목표인지
+    private Uploadable uploadable; // 인증 가능 조건
+    private String goalSchedule; // 목표 수행 일정
+    private String mateSchedule; // 팀원의 목표 수행 일정
+    private long progress; // 팀원의 목표 수행 진행률
 }
