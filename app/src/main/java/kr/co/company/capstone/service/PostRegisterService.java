0b37f9d315831ce68bf9c7d0f3b098b03f947d45
김상익 <ksik518@naver.com>
2023-06-06 21:06:59 +0900

    PostRegisterService Interface 리팩토링 및 메소드 시그니처 업데이트

--- app/src/main/java/kr/co/company/capstone/service/PostRegisterService.java
+++ app/src/main/java/kr/co/company/capstone/service/PostRegisterService.java
@@ -1,8 +1,7 @@
 package kr.co.company.capstone.service;
 
-import java.util.ArrayList;
-import java.util.Map;
 
+import android.net.Uri;
 import kr.co.company.capstone.dto.post.PostRegisterResponse;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -12,19 +11,18 @@ import retrofit2.http.POST;
 import retrofit2.http.Part;
 import retrofit2.http.PartMap;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public interface PostRegisterService {
 
     // 목표 인증
     @Multipart @POST("/posts")
-    Call<PostRegisterResponse> register(@Part("mateId") RequestBody mateId,
-                                        @Part("text") RequestBody text,
-                                        @Part MultipartBody.Part imageFile1,
-                                        @Part MultipartBody.Part imageFile2);
-
-//    Call<PostRegisterResponse> register(@PartMap Map<String, RequestBody> map,
-//                                        @Part ArrayList<MultipartBody.Part> multipartFiles);
+    Call<PostRegisterResponse> register(@PartMap Map<String, RequestBody> map,
+                                        @Part ArrayList<MultipartBody.Part> multipartFiles);
 
     static PostRegisterService getService(){
         return RetrofitBuilder.getRetrofit().create(PostRegisterService.class);
     }
+
 }
