c9f3f2cc8f4b2ea7df0cd5871a3b991ee07da97f
김상익 <ksik518@naver.com>
2023-06-08 20:32:47 +0900

    리팩토링   - 메소드 추출   - 변수명 변경

--- app/src/main/java/kr/co/company/capstone/fragment/DoMyGoalFragment.java
+++ app/src/main/java/kr/co/company/capstone/fragment/DoMyGoalFragment.java
@@ -19,10 +19,8 @@ import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
 import id.zelory.compressor.Compressor;
-import kr.co.company.capstone.util.FileTransferUtil;
 import kr.co.company.capstone.dto.ErrorMessage;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
@@ -49,7 +47,6 @@ public class DoMyGoalFragment extends Fragment {
     List<ImageView> imageViewList = new ArrayList<>();
 
     private List<Uri> imageUriList = new ArrayList<>();
-    private EditText userTextField;
     private long goalId,teamMateId;
     private String title;
     private final String LOG_TAG = "DoMyGoalFragment";
@@ -74,93 +71,61 @@ public class DoMyGoalFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_certification_detail, container, false);
 
-        ProgressBar loading = view.findViewById(R.id.do_my_goal_loading);
+        ProgressBar loading = setUpLoading(view); // 로딩 화면 초기화 및 설정
+        EditText userTextField = view.findViewById(R.id.text_field); // userTextField 초기화
 
-        loading.bringToFront();
-        loading.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.green_400), PorterDuff.Mode.SRC_IN);
-        loading.setVisibility(View.GONE);
-
-        ImageView firstIv = view.findViewById(R.id.first_image);
-        ImageView secondIv = view.findViewById(R.id.second_image);
-        ImageView thirdIv = view.findViewById(R.id.third_image);
-        userTextField = view.findViewById(R.id.text_field);
-
-        TextView goalTitle = view.findViewById(R.id.goal_title);
-        TextView todayDate = view.findViewById(R.id.today);
-
-        imageViewList.add(firstIv);
-        imageViewList.add(secondIv);
-        imageViewList.add(thirdIv);
-
-        goalTitle.setText(title);
-
-        // 현재 날짜 표시
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            todayDate.setText(LocalDate.now().toString());
-        }
-
-        // 권한 요청
-        PermissionListener permissionListener = new PermissionListener() {
-            @Override
-            public void onPermissionGranted() {
-                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "권한이 허용됨", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
-                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "권한이 거부됨", Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        TedPermission.with(getActivity())
-                .setPermissionListener(permissionListener)
-                .setRationaleMessage("카메라 권한이 필요합니다.")
-                .setDeniedMessage("카메라 권한을 거부하셨습니다.")
-                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                        Manifest.permission.CAMERA,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)
-                .check();
-
-        // 이미지 리스너 등록
-        View.OnClickListener imageSelectListener = getImageSelectListener();
-        imageViewList.forEach(iv -> iv.setOnClickListener(imageSelectListener));
-
+        // 등록 버튼 초기화
         Button registerButton = view.findViewById(R.id.Post);
 
+        // ImageView 초기화
+        initializeImageViews(view);
+
+        // textView 설정 (목표 이름, 오늘 날짜)
+        setTextViews(view);
+
+
+        //카메라 권한을 요청 및 처리
+        //요청 허용, 거부에 따른 Toast 메시지
+        requestCameraPermission();
+
+
+        // 이미지 리스너 등록
+        setImageSelectListener();
+
         // 등록 버튼 리스너
+        clickRegisterButton(loading, userTextField, registerButton);
+
+        return view;
+    }
+
+    // 이미지 리스너 등록
+    private void setImageSelectListener() {
+        View.OnClickListener imageSelectListener = getImageSelectListener();
+        imageViewList.forEach(iv -> iv.setOnClickListener(imageSelectListener));
+    }
+
+    // 등록 버튼 리스너
+    private void clickRegisterButton(ProgressBar loading, EditText userTextField, Button registerButton) {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @SneakyThrows
             @Override
             public void onClick(View view) {
+
                 // 로딩 표시
                 loading.setVisibility(View.VISIBLE);
 
-                // 사용자 입력 Text 가져오기
-                String userInputText = userTextField.getText().toString();
-
-                // MultipartBody.Part 생성 ( 이미지 파일 업로드 )
-                ArrayList<MultipartBody.Part> imageList = new ArrayList<>();
-                for(Uri uri : imageUriList) {
-                    String path = getImageFilePath(uri);
-
-                    // 이미지 압축
-                    RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), getCompressedImage(path));
-                    MultipartBody.Part uploadFile = MultipartBody.Part.createFormData("images",
-                            getOriginalImageFilename(path), requestFile);
-                    imageList.add(uploadFile);
-                }
-
                 // 필드값 삽입
-                Map<String, RequestBody> map = new HashMap<>();
+                // Map 에 mateId 와 사용자 입력 텍스트 설정
+                Map<String, RequestBody> map = putFiledValueToMap();
 
-                RequestBody mateIdFiled = RequestBody.create(MediaType.parse("text/plain"), String.valueOf(teamMateId));
+                // 등록 (API 호출)
+                registerUserData(view, map);
 
-                RequestBody textDataField = RequestBody.create(MediaType.parse("text/plain"), userInputText);
-                map.put("mateId", mateIdFiled);
-                map.put("content", textDataField);
+            }
 
-                // 포스트 API 호출
-                PostRegisterService.getService().register(map, imageList)
+
+            private void registerUserData(View view, Map<String, RequestBody> map) throws IOException {
+                PostRegisterService.getService().register(map, getImages(imageUriList))
                         .enqueue(new Callback<PostRegisterResponse>() {
                             @SneakyThrows
                             @Override
@@ -168,8 +133,10 @@ public class DoMyGoalFragment extends Fragment {
                                 if (response.isSuccessful()) {
                                     // 등록 성공시 타임라면 화면으로 이동
                                     TimeLineFragment timeLineFragment = new TimeLineFragment();
+
                                     Bundle next = new Bundle();
                                     next.putLong("goalId", goalId);
+
                                     timeLineFragment.setArguments(next);
                                     Navigation.findNavController(view).navigate(R.id.action_doMyGoalFragment_to_timeLineFragment, next);
                                 } else {
@@ -187,13 +154,98 @@ public class DoMyGoalFragment extends Fragment {
                             }
                         });
             }
-        });
 
-        return view;
+            // Map 에 mateId 와 사용자 입력 텍스트 설정
+            @NotNull
+            private Map<String, RequestBody> putFiledValueToMap() {
+                Map<String, RequestBody> map = new HashMap<>();
+
+                String userInputText = userTextField.getText().toString();
+
+                RequestBody mateIdFiled = RequestBody.create(MediaType.parse("text/plain"), String.valueOf(teamMateId));
+                RequestBody textDataField = RequestBody.create(MediaType.parse("text/plain"), userInputText);
+
+                map.put("mateId", mateIdFiled);
+                map.put("content", textDataField);
+
+                return map;
+            }
+        });
     }
 
-    private List<MultipartBody.Part> getImages(List<Uri> imageUriList) throws IOException {
-        List<MultipartBody.Part> imageParts = new ArrayList<>();
+    // Permission Listener 생성
+    @NotNull
+    private PermissionListener setPermissionListener() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "권한이 허용됨", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "권한이 거부됨", Toast.LENGTH_SHORT).show();
+            }
+        };
+        return permissionListener;
+    }
+
+    // 카메라 권한을 요청 및 처리
+    private void requestCameraPermission() {
+
+        // Permission Listener 생성
+        PermissionListener permissionListener = setPermissionListener();
+
+        // 권한 요청
+        TedPermission.with(getActivity())
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("카메라 권한이 필요합니다.")
+                .setDeniedMessage("카메라 권한을 거부하셨습니다.")
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    // 로딩 표시 초기화 및 설정
+    private ProgressBar setUpLoading(View view){
+        ProgressBar loading = view.findViewById(R.id.do_my_goal_loading);
+
+        loading.bringToFront();
+        loading.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.green_400), PorterDuff.Mode.SRC_IN);
+        loading.setVisibility(View.GONE);
+
+        return loading;
+    }
+
+    // TextView 설정
+    private void setTextViews(View view) {
+        TextView goalTitle = view.findViewById(R.id.goal_title); // 목표 이름
+        TextView todayDate = view.findViewById(R.id.today); // 오늘 날짜 표시
+
+
+        goalTitle.setText(title);
+
+        // 현재 날짜 표시
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            todayDate.setText(LocalDate.now().toString());
+        }
+    }
+
+    // ImageView 초기화
+    private void initializeImageViews(View view) {
+        ImageView firstIv = view.findViewById(R.id.first_image);
+        ImageView secondIv = view.findViewById(R.id.second_image);
+        ImageView thirdIv = view.findViewById(R.id.third_image);
+
+        imageViewList.add(firstIv);
+        imageViewList.add(secondIv);
+        imageViewList.add(thirdIv);
+    }
+
+    // Image URI 목록을 받아와 압축 후, MultipartBody.Part 형태로 변환
+    private ArrayList<MultipartBody.Part> getImages(List<Uri> imageUriList) throws IOException {
+        ArrayList<MultipartBody.Part> imageParts = new ArrayList<>();
         for (Uri imageUri : imageUriList) {
             File imageFile = new File(imageUri.getPath());
             File compressedImageFile = new Compressor(getActivity().getApplicationContext()).compressToFile(imageFile);
@@ -204,24 +256,6 @@ public class DoMyGoalFragment extends Fragment {
             imageParts.add(imagePart);
         }
         return imageParts;
-    }
-
-    // 이미지 파일 경로 받아와, 해당 이미지 압축
-    private File getCompressedImage(String path) throws IOException {
-        return new Compressor(getActivity().getApplicationContext()).compressToFile(new File(path));
-    }
-
-    // 기존 파일명 추출
-    private String getOriginalImageFilename(String path) {
-        String[] split = path.split("[/]");
-        String filename = split[split.length - 1];
-        return filename;
-    }
-
-    // Image URI -> 파일 경로 추출
-    @Nullable
-    private String getImageFilePath(Uri uri) {
-        return FileTransferUtil.getPath(uri, getActivity());
     }
 
     @NotNull
@@ -243,6 +277,7 @@ public class DoMyGoalFragment extends Fragment {
                         .showMultiImage(new TedBottomSheetDialogFragment.OnMultiImageSelectedListener() {
                             @Override
                             public void onImagesSelected(List<Uri> uriList) {
+                                // 이미지 선택 시 콜백 메소드
                                 imageUriList = new ArrayList<>();
                                 imageViewList.forEach(iv -> iv.setImageResource(0));
                                 IntStream.range(0, uriList.size()).forEach(
@@ -255,6 +290,8 @@ public class DoMyGoalFragment extends Fragment {
                         });
             }
         };
+
         return imageSelectListener;
     }
+
 }
