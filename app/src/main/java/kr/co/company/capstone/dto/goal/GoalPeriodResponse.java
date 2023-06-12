cf3c2ea691e1e30246252867aed0a175efa5e779
김상익 <ksik518@naver.com>
2023-06-12 21:45:30 +0900

    GoalPeriodResponse 주석

--- app/src/main/java/kr/co/company/capstone/dto/goal/GoalPeriodResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/goal/GoalPeriodResponse.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class GoalPeriodResponse {
-    private String startDate;
-    private String endDate;
-    private String schedule;
+    private String startDate; // 시작일
+    private String endDate;   // 종료일
+    private String schedule;  // 인증일(1 이면 인증요일, 0 이면 해당 없음)
 }
