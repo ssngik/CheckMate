9172d6ceec8d70415c41e75f5c388661bbb7bba5
김상익 <ksik518@naver.com>
2023-06-12 22:17:37 +0900

    필드명 변경, 주석 추가   - likedUsers -> likedUserIds   - text -> content

--- app/src/main/java/kr/co/company/capstone/dto/post/PostInquiryResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/post/PostInquiryResponse.java
@@ -10,12 +10,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PostInquiryResponse {
-    private long postId;
-    private long teamMateId;
-    private String uploaderNickname;
-    private String uploadAt;
-    private List<String> imageUrls;
-    private List<Long> likedUsers;
-    private String text;
-
+    private long postId;             // 포스트ID
+    private long teamMateId;         // 업로더의 팀원 ID
+    private String uploaderNickname; // 업로더의 닉네임
+    private String uploadAt;         // 업로드 시간
+    private List<String> imageUrls;  // 이미지 파일 접근 주소
+    private String content;          // 텍스트 인증 내용
+    private List<Long> likedUserIds;   // 좋아요 누른 유저 ID
 }
