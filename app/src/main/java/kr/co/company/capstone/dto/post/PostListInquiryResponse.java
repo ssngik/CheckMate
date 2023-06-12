359a6e9df382eea6265bcf797e03befff5dd5aff
김상익 <ksik518@naver.com>
2023-06-12 22:19:52 +0900

    PostListInquiryResponse 필드 변경   - minimumLike 삭제

--- app/src/main/java/kr/co/company/capstone/dto/post/PostListInquiryResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/post/PostListInquiryResponse.java
@@ -7,6 +7,5 @@ import lombok.Data;
 @Data
 public class PostListInquiryResponse {
     private List<PostInquiryResponse> posts;
-    private Integer minimumLike;
-    private String goalTitle;
+    private String goalTitle; // 해당 목표의 이름
 }
