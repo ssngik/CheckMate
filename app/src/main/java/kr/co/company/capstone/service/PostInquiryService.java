75f81563c7503bae32e82ff573b8503c3172ee83
김상익 <ksik518@naver.com>
2023-06-12 22:21:03 +0900

    PostInquiryService 인터페이스:   - getPosts 메소드 수정 -> 게시글 조회 복구

--- app/src/main/java/kr/co/company/capstone/service/PostInquiryService.java
+++ app/src/main/java/kr/co/company/capstone/service/PostInquiryService.java
@@ -4,11 +4,12 @@ import kr.co.company.capstone.dto.post.PostListInquiryResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface PostInquiryService {
-    @GET("/post")
-    Call<PostListInquiryResponse> getPosts(@Query("goalId") long goalId, @Query("date") String date);
+
+    // 목표의 해 날짜 게시글 조회
+    @GET("/goals/{goalId}/posts/{date}")
+    Call<PostListInquiryResponse> getPosts(@Path("goalId") long goalId, @Path("date") String date);
 
     static PostInquiryService getService(){
         return RetrofitBuilder.getRetrofit().create(PostInquiryService.class);
