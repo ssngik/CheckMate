5042e37fe31758fb4428c8323f525ee0e868681a
김상익 <ksik518@naver.com>
2023-06-20 16:48:06 +0900

    좋아요 관련 Interface 앤드포인트 변경

--- app/src/main/java/kr/co/company/capstone/service/LikeService.java
+++ app/src/main/java/kr/co/company/capstone/service/LikeService.java
@@ -8,12 +8,13 @@ import retrofit2.http.Path;
 
 public interface LikeService {
     // 좋아요
-    @POST("/posts/{postId}/like")
-    Call<Void> like(@Path("postId")long postId);
+    // /goals/{goalId}/posts/{postId}/like
+    @POST("/goals/{goalId}/posts/{postId}/like")
+    Call<Void> like(@Path("goalId") long  goalId ,@Path("postId")long postId);
 
     // 좋아요 취소
-    @DELETE("/posts/{postId}/unlike")
-    Call<Void> unlike(@Path("postId")long postId);
+    @DELETE("/goals/{goalId}/posts/{postId}/unlike")
+    Call<Void> unlike(@Path("goalId") long  goalId, @Path("postId")long postId);
 
     static LikeService getService(){
         return RetrofitBuilder.getRetrofit().create(LikeService.class);
