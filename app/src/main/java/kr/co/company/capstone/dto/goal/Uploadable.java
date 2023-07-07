b1d42c9a5b65cf647921c8d9f9fdb2b2caf89e76
김상익 <ksik518@naver.com>
2023-07-07 23:09:01 +0900

    @ToString 어노테이션 추가

--- app/src/main/java/kr/co/company/capstone/dto/goal/Uploadable.java
+++ app/src/main/java/kr/co/company/capstone/dto/goal/Uploadable.java
@@ -5,9 +5,12 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@ToString
 public class Uploadable {
     private boolean uploaded; // 이미 인증했는지
     private boolean uploadable; // 목표를 조회한 유저가 목표를 인증할 수 있는지
     private boolean workingDay; // 인증한 날이 맞는지
     private boolean timeOver; // 인증 시간 초과 여부
 }
+
+
