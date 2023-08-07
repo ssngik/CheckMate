81b04e0296a97bd5bf941ad3131c5d9d8da776be
김상익 <ksik518@naver.com>
2023-08-08 01:29:53 +0900

    목표 캘린더 조회 Response field 수정

--- app/src/main/java/kr/co/company/capstone/dto/goal/TeamMateCalendarResponse.java
+++ app/src/main/java/kr/co/company/capstone/dto/goal/TeamMateCalendarResponse.java
@@ -7,11 +7,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+// 목표 캘린더 조회
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class TeamMateCalendarResponse {
-    private String startDate;
-    private String goalPeriod;
-    private String teamMatePeriod;
+    private String startDate; // 목표 수행 시작일
+    private String goalPeriod; // 목표 수행 종료일
+    private String goalSchedule;  // 목표 수행 일정  ( 목표 수행일 해당 여부 )
+    private String mateSchedule; // 팀원의 목표 수행 일정  ( 목표를 수행했는지에 대한 여부 )
 }
