bd0524779eb0b46d513dbc95af66c738699c8e6d
김상익 <ksik518@naver.com>
2024-02-20 00:17:05 +0900

    refactor : Bundle 데이터 입력 방식 변경 목표 수행 날짜 start_date, end_date 분리

--- app/src/main/java/kr/co/company/capstone/fragment/CreateNewGoalFirstPage.kt
+++ app/src/main/java/kr/co/company/capstone/fragment/CreateNewGoalFirstPage.kt
@@ -10,29 +10,27 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.google.android.material.datepicker.MaterialDatePicker
 import kr.co.company.capstone.R
-import kr.co.company.capstone.databinding.FragmentCreateGoalInfoBinding
-import kr.co.company.capstone.dto.goal.GoalDetailResponse
-import kr.co.company.capstone.dto.team_mate.TeamMatesResponse
+import kr.co.company.capstone.databinding.FragmentCreateNewGoalFirstPageBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 class CreateNewGoalFirstPage : Fragment() {
 
     // 바인딩 객체 타입 null을 허용  ( onDestroy 시 완벽 제거 )
-    private var _binding: FragmentCreateGoalInfoBinding? = null
+    private var _binding: FragmentCreateNewGoalFirstPageBinding? = null
 
     // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
     private val binding get() = _binding!!
 
-    private lateinit var goal: GoalDetailResponse<TeamMatesResponse>
+    private lateinit var startDate : String
+    private lateinit var endDate : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (arguments != null) {
-            val bundle = arguments!!
-            goal =
-                bundle.getSerializable("goalDetailResponse") as GoalDetailResponse<TeamMatesResponse>
-        }
 
         super.onCreate(savedInstanceState)
     }
@@ -42,7 +40,7 @@ class CreateNewGoalFirstPage : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentCreateGoalInfoBinding.inflate(inflater, container, false)
+        _binding = FragmentCreateNewGoalFirstPageBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -60,6 +58,9 @@ class CreateNewGoalFirstPage : Fragment() {
 
         // MaterialDatepicker 초기화 / 리스너 설정
         setDatepicker()
+
+        // 다음 페이지로 이동 (다음 버튼 클릭 리스너)
+        actionToSecondPage()
     }
 
     // 목표 이름 Text Changed 리스너 설정
@@ -105,26 +106,27 @@ class CreateNewGoalFirstPage : Fragment() {
     // MaterialDatepicker 초기화 / 리스너 설정
     private fun setDatepicker() {
         binding.calendarIcon.setOnClickListener {
+
             val datePicker = MaterialDatePicker.Builder.dateRangePicker().build()
             datePicker.show(parentFragmentManager, "MaterialDatePicker")
 
-            datePicker.addOnPositiveButtonClickListener {
-                onDateSelected(datePicker.headerText)
+            datePicker.addOnPositiveButtonClickListener {dateRange ->
+                val selection = Pair(dateRange.first, dateRange.second)
+                dateFormat(selection)
+
+                binding.datePickerData.text = datePicker.headerText
                 binding.datePickerData.setTextAppearance(R.style.fill_text)
+
                 setButtonEnabledIfAllSet()
             }
-        }
-    }
 
-    // 날짜 선택을 처리하는 함수
-    private fun onDateSelected(selectedDate: String) {
-        binding.datePickerData.text = selectedDate
+        }
     }
 
     // 필드 입력에 따른 버튼 활성화 변경
     private fun setButtonEnabledIfAllSet() {
         val fieldsAreSet = areRequiredFields()
-        val drawable = if (fieldsAreSet) {
+        val drawable = if (areRequiredFields()) {
             R.drawable.btn_main
         } else {
             R.drawable.btn_main_disabled
@@ -132,6 +134,7 @@ class CreateNewGoalFirstPage : Fragment() {
 
         setButtonDrawable(drawable, fieldsAreSet)
     }
+
 
     // 모든 필드 상태 확인
     private fun areRequiredFields(): Boolean {
@@ -152,6 +155,14 @@ class CreateNewGoalFirstPage : Fragment() {
         return true
     }
 
+    // Field 조건에 맞는 Date 형식 Format
+    private fun dateFormat(selection: Pair<Long, Long>){
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+        startDate = simpleDateFormat.format(Date(selection.first))
+        endDate = simpleDateFormat.format(Date(selection.second))
+    }
+
     // 필드 입력에 따른 버튼 색상 변경
     private fun setButtonDrawable(drawableRes: Int, isEnabled: Boolean) {
         val drawable = ResourcesCompat.getDrawable(resources, drawableRes, null)
@@ -159,12 +170,25 @@ class CreateNewGoalFirstPage : Fragment() {
         binding.saveBtn.isEnabled = isEnabled
     }
 
+    // 목표 생성 2페이지로 이동
+    private fun actionToSecondPage(){
+        binding.saveBtn.setOnClickListener {
+            if (areRequiredFields()){
+                val firstPageBundle = Bundle().apply {
+                    putString("startDate", startDate)
+                    putString("endDate", endDate)
+                    putString("category", binding.spinner.selectedItem.toString())
+                    putString("title", binding.title.text.toString())
+                }
+                Navigation.findNavController(binding.root).navigate(R.id.action_createNewGoalFirstPage_to_createNewGoalFinalPage, firstPageBundle)
+            }
+        }
+    }
 
     override fun onDestroyView() {
         // binding class 인스턴스 참조 정리
         _binding = null
         super.onDestroyView()
     }
-
 
 }
