4e9df21796c74a6cf67a3a900652678cbbd938c3
김상익 <ksik518@naver.com>
2024-03-15 22:33:12 +0900

    refactor : Convert Java File to Kotlin File

--- app/src/main/java/kr/co/company/capstone/activity/SplashActivity.kt
+++ app/src/main/java/kr/co/company/capstone/activity/SplashActivity.kt
@@ -1,49 +1,45 @@
-package kr.co.company.capstone.activity;
+package kr.co.company.capstone.activity
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.company.capstone.R
+import kr.co.company.capstone.Theme
+import kr.co.company.capstone.util.SharedPreferenceUtil
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import kr.co.company.capstone.R;
-import kr.co.company.capstone.util.SharedPreferenceUtil;
-import kr.co.company.capstone.Theme;
-
-public class SplashActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+class SplashActivity : AppCompatActivity() {
+    private val splashDelay = 2000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         // accessToken 가져오기
-        String accessToken = SharedPreferenceUtil.getString(this, "accessToken");
-
+        val accessToken = SharedPreferenceUtil.getString(this, "accessToken")
         //로딩화면 시작.
-        LoadingStart(accessToken);
+        LoadingStart(accessToken)
     }
-    private void LoadingStart(String accessToken){
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable(){
-            public void run(){
-                if(accessToken.isEmpty()) { // accessToken 만료
-                    SharedPreferenceUtil.removeKey(getApplicationContext(), "accessToken");
 
-                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                    Log.d("splash", "loading done !");
-                }
-                else {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    // 화면 테마 적용
-                    Theme.applyTheme(SharedPreferenceUtil.getString(getApplicationContext(), "scnMode"));
-                }
+    private fun LoadingStart(accessToken: String) {
+        Handler().postDelayed({
+            if (accessToken.isEmpty()) { // accessToken 만료
+                navigateToLoginActivity()
+            } else {
+                navigateToMainActivity()
             }
-        },2000);
+            finish()
+        }, splashDelay)
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToLoginActivity() {
+        SharedPreferenceUtil.removeKey(applicationContext, "accessToken")
+        val intent = Intent(applicationContext, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
