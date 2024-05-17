f64c5215580128bc26411157ef065199f5e95b1b
김상익 <ksik518@naver.com>
2024-05-17 23:42:33 +0900

    feat : FragmentUtil 클래스 추가
    Navigation action 관리 Fragment Util Class 추가

---
+++ app/src/main/java/kr/co/company/capstone/util/FragmentUtil.kt
@@ -0,0 +1,22 @@
+package kr.co.company.capstone.util
+
+import android.os.Bundle
+import android.view.View
+import kr.co.company.capstone.R
+
+class FragmentUtil {
+    fun actionDetailToInvite(view : View, goalId:Long){
+        val bundle = Bundle()
+        bundle.putLong("goalId", goalId)
+        NavigationUtil.navigateTo(view, R.id.action_goalDetailFragment_to_inviteUserFragment, bundle)
+    }
+
+    fun actionDetailToTimeLine(view: View, goalId: Long){
+        NavigationUtil.navigateTo(view, R.id.action_goalDetailFragment_to_timeLineFragment)
+    }
+
+    fun actionDetailToDoMyGoal(view: View){
+        NavigationUtil.navigateTo(view, R.id.action_goalDetailFragment_to_doMyGoalFragment)
+    }
+
+}
