d1262aaa67ef8bffc462ba561b6d7104ac3e6f39
김상익 <ik741953@gmail.com>
2025-01-11 21:26:05 +0900

    [CMA-8] refactor : 불필요 파일 정리
    - 불필요 클래스 및 메소드 삭제

--- app/src/main/java/kr/co/company/capstone/service/GoalCreateService.java
+++ app/src/main/java/kr/co/company/capstone/service/GoalCreateService.java
@@ -1,22 +1,15 @@
 package kr.co.company.capstone.service;
 
 import kr.co.company.capstone.dto.goal.MakeGoalResponse;
-import kr.co.company.capstone.dto.goal.GoalModifyRequest;
 import kr.co.company.capstone.dto.goal.MakeGoalRequest;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.PATCH;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 public interface GoalCreateService {
     // 목표 생성
     @POST("/goals")
     Call<MakeGoalResponse> saveGoal(@Body MakeGoalRequest makeGoalRequest);
-
-    // 목표 수정
-    @PATCH("/goals/{goalId}")
-    Call<Void> modifyGoal(@Path("goalId") long goalId, @Body GoalModifyRequest goalModifyRequest);
 
     static GoalCreateService getService(){
         return RetrofitBuilder.INSTANCE.getRetrofit().create(GoalCreateService.class);
