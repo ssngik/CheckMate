133dee171572a468cea496942742bf6c592bd37a
김상익 <ik741953@gmail.com>
2025-01-17 19:35:55 +0900

    [CMA-10] feat : AlertDialogFragment에 두 개의 버튼 선택 기능 추가
    - 두 개의 버튼 (확인 및 취소)을 표시 가능한 기능 추가.
    - 단일과 두 개의 버튼을 조건에 따라 표시되도록 수정
    - 버튼 텍스트 커스터미이징을 위한 positiveButtonText, negativeButtonText 추가
    - 버튼 클릭 시 동작 onPositiveAction, onNegativeAction 리스너 구현

--- app/src/main/java/kr/co/company/capstone/fragment/AlertDialogFragment.kt
+++ app/src/main/java/kr/co/company/capstone/fragment/AlertDialogFragment.kt
@@ -10,10 +10,14 @@ import kr.co.company.capstone.R
 import kr.co.company.capstone.databinding.FragmentCustomDialogBinding
 
 class AlertDialogFragment(
-    val title: String,
-    val body: String,
+    private val title: String,
+    private val body: String,
     private val emojiStatus: Boolean,
-    private val onPositiveAction: (() -> Unit)? = null
+    private val positiveButtonText: String = "확인",
+    private val negativeButtonText: String? = null,
+    private val onPositiveAction: (() -> Unit)? = null,
+    private val onNegativeAction: (() -> Unit)? = null
+
 ) : DialogFragment(){
     private var _binding: FragmentCustomDialogBinding? = null
     private val binding get() = _binding!!
@@ -28,19 +32,43 @@ class AlertDialogFragment(
         binding.dialogBody.text = body
 
         setEmoji(binding.emoji)
-        setButtonClickListener()
+        setButtonVisibilityAndListeners()
         return binding.root
     }
 
     private fun setEmoji(emoji: ImageView) {
-        if (emojiStatus) emoji.setImageResource(R.drawable.emojis_smiling_face)
-        else emoji.setImageResource(R.drawable.emojis_frowning_face)
+        val emojiResource = if (emojiStatus) R.drawable.emojis_smiling_face else R.drawable.emojis_frowning_face
+        emoji.setImageResource(emojiResource)
     }
 
-    private fun setButtonClickListener() {
-        binding.btnOk.setOnClickListener {
-            onPositiveAction?.invoke()
-            dismiss()
+    private fun setButtonVisibilityAndListeners() {
+        if (negativeButtonText == null) {
+            binding.btnOk.visibility = View.VISIBLE
+            binding.buttonContainer.visibility = View.GONE
+
+            binding.btnOk.text = positiveButtonText
+            binding.btnOk.setOnClickListener {
+                dismiss()
+                onPositiveAction?.invoke()
+            }
+        } else {
+            // 두 개의 버튼 표시
+            binding.btnOk.visibility = View.GONE
+            binding.buttonContainer.visibility = View.VISIBLE
+
+            // 취소 버튼 설정
+            binding.btnSelectCancel.text = negativeButtonText
+            binding.btnSelectCancel.setOnClickListener {
+                dismiss()
+                onNegativeAction?.invoke()
+            }
+
+            // 확인 버튼 설정
+            binding.btnSelectOk.text = positiveButtonText
+            binding.btnSelectOk.setOnClickListener {
+                dismiss()
+                onPositiveAction?.invoke()
+            }
         }
     }
 
